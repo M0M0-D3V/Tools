4a40cc6e088e648782cfee6aa49b792e61ec99d7
momoxstarx <momo.star@gmail.com>
2021-09-08 21:16:57 -0700

    pretty

--- FE3H/ClassUnlockable/script.js
+++ FE3H/ClassUnlockable/script.js
@@ -9,6 +9,7 @@ let classesLength = classesJSON.length
 
 for (let i = 0; i < classesLength; i++) {
     let newOption = document.createElement("option")
+    newOption.setAttribute("value", i)
     newOption.innerHTML = classesJSON[i]["Class Name"]
     selectNames.appendChild(newOption)
 }
@@ -19,5 +20,28 @@ window.work = function work() {
 }
 
 window.showClass = function showClass() {
-    console.log("triggered")
+    let selected = document.getElementById("class-names").selectedIndex;
+    let idNum = document.getElementsByTagName("option")[selected].value
+    let displayClass = document.getElementById("displayClass")
+    let thisClass = classesJSON[idNum]
+    displayClass.innerHTML = "<h5>Class Name:</h5>"
+    displayClass.innerHTML += `<p>${thisClass["Class Name"]}</p>`
+
+    displayClass.innerHTML += "<h5>Certification:</h5>"
+    displayClass.innerHTML += `<p>${thisClass["Certification"]}</p>`
+
+    displayClass.innerHTML += "<h5>Level Seal:</h5>"
+    displayClass.innerHTML += `<p>${thisClass["Level Seal"]}</p>`
+
+    displayClass.innerHTML += "<h5>Proficiency:</h5>"
+    displayClass.innerHTML += `<p>${thisClass["Proficiency"]}</p>`
+    
+    displayClass.innerHTML += "<h5>Abilities:</h5>"
+    displayClass.innerHTML += `<p>${thisClass["Abilities"]}</p>`
+    
+    displayClass.innerHTML += "<h5>Mastery Abilities:</h5>"
+    displayClass.innerHTML += `<p>${thisClass["Mastery Abilities"]}</p>`
+    
+    displayClass.innerHTML += "<h5>Specific:</h5>"
+    displayClass.innerHTML += `<p>${thisClass["Specific"]}</p>`
 }
