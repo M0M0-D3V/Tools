8bdcef2acba44afc69a40560326a11aff603fd93
momoxstarx <momo.star@gmail.com>
2021-09-18 22:03:02 -0700

    finish design

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,21 @@
+import {toolsJSON} from "./toolsJSON.js"
+
 console.log(`Hello my favorite human, I am here to serve you.`)
 
+let contentArea = document.getElementById("content-area")
 let infoArea = document.getElementById("info-blurb")
+let numberOfTools = toolsJSON.length
 
-window.showD1 = function showD1() {
-    infoArea.innerHTML = `<h3 class="info">hello this is my first tool project</h3>`
+// POPULATE THE CONTENT AREA WITH TOOL NAMES
+for(let i = 0; i < numberOfTools; i++) {
+    let thisTool = toolsJSON[i]
+    let title = document.createElement("h3")
+    title.setAttribute("onmouseover", `showTool(${i})`)
+    title.innerHTML = `Day ${thisTool["Days"]} - <a href="${thisTool["Link"]}" target="_blank" rel="noopener noreferrer">${thisTool["Tool Name"]}</a>`
+    contentArea.append(title)
 }
 
-window.showD2to3 = function showD2to3() {
-    infoArea.innerHTML = `<h3 class="info">hello this is my second tool project</h3>`
-}
-
-window.showD4to14 = function showD4to14() {
-    infoArea.innerHTML = `<h3 class="info">hello this is my third tool project that took a really long time for some reason but i'm happy it is done!</h3>`
-}
-
-window.showD5 = function showD5() {
-    infoArea.innerHTML = `<h3 class="info">hello this is my fourth tool project</h3>`
-}
-
-window.showD15 = function showD15() {
-    infoArea.innerHTML = `<h3 class="info">hello this is my current tool project</h3>`
+// FUNCTION TO DISPLAY DESCRIPTION ON MOUSEHOVER
+window.showTool = function showTool(index) {
+    infoArea.innerHTML = `<h3 class="info">${toolsJSON[index]["Description"]}</h3>`
 }
---
+++ static/js/toolsJSON.js
@@ -0,0 +1,34 @@
+const toolsJSON = [
+    {
+        "Tool Name": "Ratio Calculator",
+        "Description": "My husband is always needing to calculate ratios and pixels for his work as an animator so I made this tool that would help him with just a simple cross-multiplication function in HTML/CSS/Javascript",
+        "Link": "RatioCalculator/index.html",
+        "Days": "1",
+    },
+    {
+        "Tool Name": "Batch Renamer for Maya Autodesk",
+        "Description": "Made a tool for my husband that can help him save alot of time of renaming all objects in a Maya file. I wrote a script in Python and then later converted to MEL so that he can use it directly in his Autodesk environment.",
+        "Link": "MEL_Scripts/RenameTextInFile/scriptfull.py",
+        "Days": "2, 3",
+    },
+    {
+        "Tool Name": "Fire Emblem 3 Houses - Class Matcher",
+        "Description": "This is a tool I made for my personal use as I am playing through a tactical strategy game. The game has various characters to level and to have classes unlocked at a certain skill level. I got tired of looking up screen to screen and decided to make a a button that will match all the available classes to the skills and stats entered. It was a slow process of 10 days and I think it was worth it. Made with HTML/CSS/Javascript",
+        "Link": "FE3H/ClassUnlockable/index.html",
+        "Days": "4 - 14",
+    },
+    {
+        "Tool Name": "CSV Visualizer",
+        "Description": "This is a side tool I made to help with building the prior Class Matcher tool. I didn't want to spend time entering to code so I made a form to take in a spreadsheet and visualize the data as well as extract into a JSON file. Made with HTML/CSS/Javascript.",
+        "Link": "CSVtoJSON/index.html",
+        "Days": "5",
+    },
+    {
+        "Tool Name": "Designed this landing page",
+        "Description": "After finishing the Fire Emblem Class Matcher tool, I wanted to take a load off and do something fun with HTML/CSS/Javascript so that this page is more alive to you. :)",
+        "Link": "#",
+        "Days": "15",
+    },
+]
+
+export { toolsJSON }
