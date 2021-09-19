5be7488081ef1f13d96b2463812d23b9cd0f1cbe
momoxstarx <momo.star@gmail.com>
2021-09-18 17:22:50 -0700

    landing page

---
+++ static/js/script.js
@@ -0,0 +1,23 @@
+console.log(`Hello my favorite human, I am here to serve you.`)
+
+let infoArea = document.getElementById("info-blurb")
+
+window.showD1 = function showD1() {
+    infoArea.innerHTML = `<h3 class="info">hello this is my first tool project</h3>`
+}
+
+window.showD2to3 = function showD2to3() {
+    infoArea.innerHTML = `<h3 class="info">hello this is my second tool project</h3>`
+}
+
+window.showD4to14 = function showD4to14() {
+    infoArea.innerHTML = `<h3 class="info">hello this is my third tool project that took a really long time for some reason but i'm happy it is done!</h3>`
+}
+
+window.showD5 = function showD5() {
+    infoArea.innerHTML = `<h3 class="info">hello this is my fourth tool project</h3>`
+}
+
+window.showD15 = function showD15() {
+    infoArea.innerHTML = `<h3 class="info">hello this is my current tool project</h3>`
+}
