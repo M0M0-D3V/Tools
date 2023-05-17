3f508f05b636f996eae710ae31efa9f00ea95ed4
Monica Hong <momo.star@gmail.com>
2023-05-16 23:28:49 -0700

    review jquery

--- ImageFeature/script.js
+++ ImageFeature/script.js
@@ -2,6 +2,7 @@ $(function () {
   $("#image-list").sortable({
     update: function (event, ui) {
       getAllImageIds();
+      console.log(`event ${event} - ui ${ui}`);
     },
   });
 });
@@ -9,6 +10,8 @@ $(function () {
 function getAllImageIds() {
   var values = [];
   $(".list-item").each(function (index) {
+    console.log(index);
     values.push($(this).attr("id").replace("image-", ""));
+    $("#output").attr();
   });
 }
