84a313ad7828bb20750d67965e3aa4f16d853256
Monica Hong <momo.star@gmail.com>
2025-09-20 22:30:21 -0700

    update

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,28 @@
-import {toolsJSON} from "./toolsJSON.js"
+import { toolsJSON } from "./toolsJSON.js";
 
-console.log(`Hello my favorite human, I am here to serve you.`)
+console.log(`Hello my favorite human, I am here to serve you.`);
 
-let contentArea = document.getElementById("content-area")
-let infoArea = document.getElementById("info-blurb")
-let numberOfTools = toolsJSON.length
+let contentArea = document.getElementById("content-area");
+let infoArea = document.getElementById("info-blurb");
+let numberOfTools = toolsJSON.length;
 
 // POPULATE THE CONTENT AREA WITH TOOL NAMES
-for(let i = 0; i < numberOfTools; i++) {
-    let thisTool = toolsJSON[i]
-    let title = document.createElement("h3")
-    title.setAttribute("onmouseover", `showTool(${i})`)
-    let link = ""
-    if(thisTool["Link"] == "#") {
-        link = `<a href="${thisTool["Link"]}">`
-    }
-    else {
-        link = `<a href="${thisTool["Link"]}" target="_blank" rel="noopener noreferrer">`
-    }
-    title.innerHTML = `Day ${thisTool["Days"]} - ${link}${thisTool["Tool Name"]}</a>`
-    contentArea.append(title)
+for (let i = 0; i < numberOfTools; i++) {
+  let thisTool = toolsJSON[i];
+  let title = document.createElement("h3");
+  title.setAttribute("onmouseover", `showTool(${i})`);
+  let link = "";
+  if (thisTool["Link"] == "#") {
+    link = `<a href="${thisTool["Link"]}">`;
+  } else {
+    link = `<a href="${thisTool["Link"]}" target="_blank" rel="noopener noreferrer">`;
+  }
+  // title.innerHTML = `Day ${thisTool["Days"]} - ${link}${thisTool["Tool Name"]}</a>`;
+  title.innerHTML = `${link}${thisTool["Tool Name"]}</a>`;
+  contentArea.append(title);
 }
 
 // FUNCTION TO DISPLAY DESCRIPTION ON MOUSEHOVER
 window.showTool = function showTool(index) {
-    infoArea.innerHTML = `<h3 class="info">${toolsJSON[index]["Description"]}</h3>`
-}
+  infoArea.innerHTML = `<h3 class="info">${toolsJSON[index]["Description"]}</h3>`;
+};
--- static/js/toolsJSON.js
+++ static/js/toolsJSON.js
@@ -1,58 +1,74 @@
 const toolsJSON = [
-    {
-        "Tool Name": "Ratio Calculator",
-        "Description": "My husband is always needing to calculate ratios and pixels for his work as an animator so I made this tool that would help him with just a simple cross-multiplication function in HTML/CSS/Javascript",
-        "Link": "RatioCalculator/index.html",
-        "Days": "1",
-    },
-    {
-        "Tool Name": "Batch Renamer for Maya Autodesk",
-        "Description": "Made a tool for my husband that can help him save alot of time of renaming all objects in a Maya file. I wrote a script in Python and then later converted to MEL so that he can use it directly in his Autodesk environment.",
-        "Link": "MEL_Scripts/RenameTextInFile/scriptfull.py",
-        "Days": "2, 3",
-    },
-    {
-        "Tool Name": "Fire Emblem 3 Houses - Class Matcher",
-        "Description": "This is a tool I made for my personal use as I am playing through a tactical strategy game. The game has various characters to level and to have classes unlocked at a certain skill level. I got tired of looking up screen to screen and decided to make a a button that will match all the available classes to the skills and stats entered. It was a slow process of 10 days and I think it was worth it. Made with HTML/CSS/Javascript. Edit: I'm still coming back to continue working on little things during downtime like visuals and considering adding more functionality!",
-        "Link": "FE3H/ClassUnlockable/index.html",
-        "Days": "4 - 14, 17, 19",
-    },
-    {
-        "Tool Name": "CSV Visualizer",
-        "Description": "This is a side tool I made to help with building the prior Class Matcher tool. I didn't want to spend time entering to code so I made a form to take in a spreadsheet and visualize the data as well as extract into a JSON file. Made with HTML/CSS/Javascript.",
-        "Link": "CSVtoJSON/index.html",
-        "Days": "5",
-    },
-    {
-        "Tool Name": "Designed this landing page",
-        "Description": "After finishing the Fire Emblem Class Matcher tool, I wanted to take a load off and do something fun with HTML/CSS/Javascript so that this page is more alive to you. :)",
-        "Link": "#",
-        "Days": "15",
-    },
-    {
-        "Tool Name": "Decision Helper",
-        "Description": "A tool that helps narrow down what task to do by asking a series of questions. Currently work in progress using HTML/CSS/Javascript",
-        "Link": "DecisionHelper/index.html",
-        "Days": "16",
-    },
-    {
-        "Tool Name": "APIs Pokedex",
-        "Description": "A tool to help me practice recreating the Pokedex and practicing pulling data from API. Made with HTML/CSS/Javascript",
-        "Link": "APIs/Pokemon/index.html",
-        "Days": "18",
-    },
-    {
-        "Tool Name": "Pizza API",
-        "Description": "Followed a tutorial to build out a web API for storing various pizzas in memory-cache. Wanted to touch up on API creating from scratch using C#/.NET. Will consider adding a database to it in the future for another one-off project.",
-        "Link": "#",
-        "Days": "19",
-    },
-    {
-        "Tool Name": "FriendlyChat",
-        "Description": "Followed a tutorial to learn and build a chat web app using firebase. Javascript heavy as well as reading HTML/CSS",
-        "Link": "https://friendlychat-42985.web.app/",
-        "Days": "20",
-    },
-]
+  {
+    "Tool Name": "Ratio Calculator",
+    Description:
+      "My friend is always needing to calculate ratios and pixels for his work as an animator so I made this tool that would help him with just a simple cross-multiplication function in HTML/CSS/Javascript",
+    Link: "RatioCalculator/index.html",
+    Days: "1",
+  },
+  {
+    "Tool Name": "Batch Renamer for Maya Autodesk",
+    Description:
+      "Made a tool for my friend that can help him save alot of time of renaming all objects in a Maya file. I wrote a script in Python and then later converted to MEL so that he can use it directly in his Autodesk environment.",
+    Link: "MEL_Scripts/RenameTextInFile/scriptfull.py",
+    Days: "2, 3",
+  },
+  {
+    "Tool Name": "Fire Emblem 3 Houses - Class Matcher",
+    Description:
+      "This is a tool I made for my personal use as I am playing through a tactical strategy game. The game has various characters to level and to have classes unlocked at a certain skill level. I got tired of looking up screen to screen and decided to make a a button that will match all the available classes to the skills and stats entered. It was a slow process of 10 days and I think it was worth it. Made with HTML/CSS/Javascript. Edit: I'm still coming back to continue working on little things during downtime like visuals and considering adding more functionality!",
+    Link: "FE3H/ClassUnlockable/index.html",
+    Days: "4 - 14, 17, 19",
+  },
+  {
+    "Tool Name": "CSV Visualizer",
+    Description:
+      "This is a side tool I made to help with building the prior Class Matcher tool. I didn't want to spend time entering to code so I made a form to take in a spreadsheet and visualize the data as well as extract into a JSON file. Made with HTML/CSS/Javascript.",
+    Link: "CSVtoJSON/index.html",
+    Days: "5",
+  },
+  {
+    "Tool Name": "Designed this landing page",
+    Description:
+      "After finishing the Fire Emblem Class Matcher tool, I wanted to take a load off and do something fun with HTML/CSS/Javascript so that this page is more alive to you. :)",
+    Link: "#",
+    Days: "15",
+  },
+  {
+    "Tool Name": "Decision Helper",
+    Description:
+      "A tool that helps narrow down what task to do by asking a series of questions. Currently work in progress using HTML/CSS/Javascript",
+    Link: "DecisionHelper/index.html",
+    Days: "16",
+  },
+  {
+    "Tool Name": "APIs Pokedex",
+    Description:
+      "A tool to help me practice recreating the Pokedex and practicing pulling data from API. Made with HTML/CSS/Javascript",
+    Link: "APIs/Pokemon/index.html",
+    Days: "18",
+  },
+  {
+    "Tool Name": "Pizza API",
+    Description:
+      "Followed a tutorial to build out a web API for storing various pizzas in memory-cache. Wanted to touch up on API creating from scratch using C#/.NET. Will consider adding a database to it in the future for another one-off project.",
+    Link: "#",
+    Days: "19",
+  },
+  {
+    "Tool Name": "FriendlyChat",
+    Description:
+      "Followed a tutorial to learn and build a chat web app using firebase. Javascript heavy as well as reading HTML/CSS",
+    Link: "https://friendlychat-42985.web.app/",
+    Days: "20",
+  },
+  {
+    "Tool Name": "Abyss Bingo",
+    Description:
+      "Simple Bingo Card generator for my beloved elders in my guild Azure Abyss.",
+    Link: "Bingo/index.html",
+    Days: "20",
+  },
+];
 
-export { toolsJSON }
+export { toolsJSON };
