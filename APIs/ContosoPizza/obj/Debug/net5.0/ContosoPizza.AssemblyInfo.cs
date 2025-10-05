d8ae4d14be3aa1de0a1542d6e97bcbcc6bed10d4
Monica Hong <momo.star@gmail.com>
2025-10-05 04:02:41 -0700

    fix: update body background color to match design

--- APIs/ContosoPizza/obj/Debug/net5.0/ContosoPizza.AssemblyInfo.cs
+++ APIs/ContosoPizza/obj/Debug/net5.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0bf59ccc8b1e80d374de037560c903b8c9c1e00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93451751903261d888d06b9f333fc7874a18b2f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
