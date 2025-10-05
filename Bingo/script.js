2c799b0531bf0a873413408956e73e0cd530cad1
Monica Hong <momo.star@gmail.com>
2025-10-05 06:43:09 -0700

    color changes

--- Bingo/script.js
+++ Bingo/script.js
@@ -49,20 +49,20 @@ function activateGameMode() {
   */
   const winningLines = [
     // Rows
-    [0, 1, 2, 3, 4],
-    [5, 6, 7, 8, 9],
-    [10, 11, 12, 13, 14],
-    [15, 16, 17, 18, 19],
-    [20, 21, 22, 23, 24],
+    [0, 1, 2, 3, 4], // 0
+    [5, 6, 7, 8, 9], // 1
+    [10, 11, 12, 13, 14], // 2
+    [15, 16, 17, 18, 19], // 3
+    [20, 21, 22, 23, 24], // 4
     // Columns
-    [0, 5, 10, 15, 20],
-    [1, 6, 11, 16, 21],
-    [2, 7, 12, 17, 22],
-    [3, 8, 13, 18, 23],
-    [4, 9, 14, 19, 24],
+    [0, 5, 10, 15, 20], // 5
+    [1, 6, 11, 16, 21], // 6
+    [2, 7, 12, 17, 22], // 7
+    [3, 8, 13, 18, 23], // 8
+    [4, 9, 14, 19, 24], // 9
     // Diagonals
-    [0, 6, 12, 18, 24],
-    [4, 8, 12, 16, 20],
+    [0, 6, 12, 18, 24], // 10
+    [4, 8, 12, 16, 20], // 11
   ];
 
   function checkForCompletedLines() {
@@ -96,8 +96,10 @@ function activateGameMode() {
 
       if (cell.classList.contains("marked")) {
         markedIndexes.add(index);
+        console.log(`Marked index: ${index}`);
       } else {
         markedIndexes.delete(index);
+        console.log(`Unmarked index: ${index}`);
       }
 
       // Check for winning lines
